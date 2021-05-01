965d7b80d51b307a0465eba046ebde8b7cce232a
Isaac Griffith <grifisaa@isu.edu>
2021-04-30 19:53:55 -0600

    updated

--- src/migrations/20181120060107_create_test_db.sql
+++ src/migrations/20181120060107_create_test_db.sql
@@ -59,8 +59,8 @@ create table roles
     roleKey    VARCHAR UNIQUE,
     name       VARCHAR,
     type       INTEGER,
-    mandatory  BOOLEAN,
     pattern_id INTEGER REFERENCES patterns (id),
+    mandatory  BOOLEAN,
     created_at NUMERIC,
     updated_at NUMERIC
 );
